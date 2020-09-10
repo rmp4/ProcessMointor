d1aca447bb4220e56cd87cac97010ee27ae9763d
rmp4 <atom10558@gmail.com>
2020-09-10 23:47:53 +0800

    [chore]: first commit

---
+++ processmointor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace processmointor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cmd =string.Join(" ", args); 
+            
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            Console.WriteLine("Hello World!");
+            ProcessStartInfo info = new ProcessStartInfo()
+            {
+                FileName = "/bin/bash",
+
+                Arguments = $"-c \"{escapedArgs}\"",
+                CreateNoWindow = true
+            };
+
+            using (var proc = new Process {StartInfo = info})
+            {
+                proc.Start();
+
+
+                PerformanceCounter ramCounter = new PerformanceCounter("Process", "Working Set", proc.ProcessName);
+                PerformanceCounter cpuCounter = new PerformanceCounter("Process", "% Processor Time", proc.ProcessName);
+                while (true)
+                {
+                    Thread.Sleep(500);
+                    double ram = ramCounter.NextValue();
+                    double cpu = cpuCounter.NextValue();
+                    Console.WriteLine("RAM: " + (ram / 1024 / 1024) + " MB; CPU: " + (cpu) + " %");
+                }
+            }
+        }
+    }
+}
